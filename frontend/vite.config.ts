2210133093b0a7fc71202dec1cc043d21bb28944
Denis <tankt34.stepanoff@yandex.ru>
2025-11-22 14:12:36 +0300

    fix: fix with domen

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3000
-  },
+    port: 4173,
+    allowedHosts: ['lbchresidents.ru', 'www.lbchresidents.ru'],
+},
   build: {
     outDir: 'dist',
     sourcemap: true
